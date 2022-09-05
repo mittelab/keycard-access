3bd240d8ab45978c35d9b5efb5425dad43e8831b
Pietro Saccardi <lizardm4@gmail.com>
2022-09-05 15:55:16 +0200

    Add dummy test file

---
+++ keycard-access/test/demo.cpp
@@ -0,0 +1,14 @@
+#include <unity.h>
+
+void dummy_test() {
+    TEST_ASSERT_EQUAL(42, 42);
+}
+
+extern "C" void app_main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(dummy_test);
+
+    UNITY_END();
+}
