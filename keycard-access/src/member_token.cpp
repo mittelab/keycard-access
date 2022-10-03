4396cb4848651c9e6e700f9b4a11c776a02af381
Pietro Saccardi <lizardm4@gmail.com>
2022-10-03 22:41:28 +0200

    Store root key when managing the token (needed for multiple operations)

--- keycard-access/src/member_token.cpp
+++ keycard-access/src/member_token.cpp
@@ -8,19 +8,35 @@
 #include <desfire/esp32/crypto_impl.hpp>
 
 namespace ka {
+    member_token::member_token(desfire::tag &tag) : _tag{&tag}, _root_key{desfire::key<desfire::cipher_type::des>{}} {}
+
     member_token::r<member_token::id_t> member_token::id() const {
         return tag().get_card_uid();
     }
 
-    member_token::r<key_t> member_token::get_root_key(config const &cfg) const {
-        if (const auto r = id(); r) {
-            return get_root_key(*r, cfg);
-        } else {
+    member_token::r<> member_token::try_set_root_key(desfire::any_key k) {
+        if (const auto r = tag().select_application(desfire::root_app); not r) {
             return r.error();
         }
+        if (const auto r = tag().authenticate(k); not r) {
+            return r.error();
+        }
+        set_root_key(std::move(k));
+        return mlab::result_success;
     }
 
-    key_t member_token::get_root_key(member_token::id_t token_id, config const &cfg) {
+    member_token::r<> member_token::test_root_key() const {
+        return test_root_key(root_key());
+    }
+
+    member_token::r<> member_token::test_root_key(const desfire::any_key &k) const {
+        if (const auto r = tag().select_application(desfire::root_app); not r) {
+            return r.error();
+        }
+        return tag().authenticate(k);
+    }
+
+    key_t member_token::get_default_root_key(member_token::id_t token_id, config const &cfg) {
         desfire::esp32::default_cipher_provider provider{};
         // Construct an uint8_t view over the string by casting pointers. This is accepted by bin_data
         const mlab::range<std::uint8_t const *> salt_view{
--- keycard-access/src/member_token.hpp
+++ keycard-access/src/member_token.hpp
@@ -17,6 +17,7 @@ namespace ka {
          * @note Mutable because interacting with the tag requires non-const access.
          */
         mutable desfire::tag *_tag;
+        desfire::any_key _root_key;
 
         [[nodiscard]] inline desfire::tag &tag() const;
     public:
@@ -25,11 +26,18 @@ namespace ka {
 
         using id_t = std::array<std::uint8_t, 7>;
 
-        inline explicit member_token(desfire::tag &tag);
+        explicit member_token(desfire::tag &tag);
         member_token(member_token const &) = delete;
         member_token(member_token &&) = default;
         member_token &operator=(member_token const &) = delete;
         member_token &operator=(member_token &&) = default;
+
+        [[nodiscard]] inline desfire::any_key const &root_key() const;
+        inline void set_root_key(desfire::any_key k);
+        [[nodiscard]] r<> try_set_root_key(desfire::any_key k);
+
+        [[nodiscard]] r<> test_root_key() const;
+        [[nodiscard]] r<> test_root_key(desfire::any_key const &k) const;
 
         /**
          * @brief The ID of the token, as in @ref desfire::tag::get_card_uid().
@@ -37,34 +45,33 @@ namespace ka {
         [[nodiscard]] r<id_t> id() const;
 
         /**
-         * @brief Computes the root key of the token.
+         * @brief A differentiated root key to be used with a token.
          * Note that we do not use a pre-shared key for this, rather, we simply derive an
          * token-specific key to differentiate from @ref config::master_key. The user is free to
          * tamper with their token. In the worst case, they might delete the access application
          * and need a redeploy.
-         * @see get_root_key(id_t, config const &)
-         * @return A key which gives root access to the card.
-         */
-        [[nodiscard]] r<key_t> get_root_key(config const &cfg = system_config()) const;
-
-        /**
-         * @brief A differentiated root key to be used with this token.
          * This uses @ref desfire::kdf_an10922 to differentiate @ref config::master_key into a token-specific
          * root key. It uses the @p token_id and @ref config::differentiation_salt as differentiation input data.
          * @param token_id Id of the token
          * @param cfg Current configuration
-         * @return A key.
+         * @return A key which gives root access to the card.
          */
-        [[nodiscard]] static key_t get_root_key(id_t token_id, config const &cfg = system_config());
+        [[nodiscard]] static key_t get_default_root_key(id_t token_id, config const &cfg = system_config());
     };
 
 }
 
 namespace ka {
-    member_token::member_token(desfire::tag &tag) : _tag{&tag} {}
-
     desfire::tag &member_token::tag() const {
         return *_tag;
+    }
+
+    desfire::any_key const &member_token::root_key() const {
+        return _root_key;
+    }
+
+    void member_token::set_root_key(desfire::any_key k) {
+        _root_key = std::move(k);
     }
 }
 
