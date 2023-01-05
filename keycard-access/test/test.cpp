9861e56730a928c5db33996ec99e215a84761d1c
Pietro Saccardi <lizardm4@gmail.com>
2023-01-05 17:18:21 +0100

    Fix wake test, add test for MAD (failing)

--- keycard-access/test/test.cpp
+++ keycard-access/test/test.cpp
@@ -2,6 +2,7 @@
 #include <desfire/esp32/cipher_provider.hpp>
 #include <desfire/tag.hpp>
 #include <ka/keypair.hpp>
+#include <ka/member_token.hpp>
 #include <pn532/controller.hpp>
 #include <pn532/desfire_pcd.hpp>
 #include <pn532/esp32/hsu.hpp>
@@ -14,6 +15,36 @@ using namespace std::chrono_literals;
 static const unsigned char plaintext[] = "The quick brown fox jumps over the lazy dog";
 
 namespace ut {
+
+    void test_encrypt_decrypt() {
+        const mlab::bin_data message = mlab::bin_data::chain(plaintext);
+        mlab::bin_data buffer = message;
+
+        key_pair k1, k2;
+        k1.generate();
+        k2.generate();
+
+        TEST_ASSERT(k1.is_valid());
+        TEST_ASSERT(k2.is_valid());
+
+        TEST_ASSERT(k1.encrypt_for(k2, buffer));
+        TEST_ASSERT(k2.decrypt_from(k1, buffer));
+
+        TEST_ASSERT_EQUAL(buffer.size(), message.size());
+        TEST_ASSERT_EQUAL_HEX8_ARRAY(message.data(), buffer.data(), std::min(message.size(), buffer.size()));
+    }
+
+    void test_keys() {
+        key_pair k;
+        TEST_ASSERT(not k.is_valid());
+
+        k.generate();
+        TEST_ASSERT(k.is_valid());
+
+        key_pair const k2{k.raw_sk()};
+        TEST_ASSERT(k2.is_valid());
+        TEST_ASSERT(k.raw_pk() == k2.raw_pk());
+    }
 
     namespace {
         /**
@@ -30,6 +61,9 @@ namespace ut {
         /**
          * @}
          */
+
+        constexpr auto test_holder = "user";
+        constexpr auto test_publisher = "Mittelab";
     }
 
     template <class Result>
@@ -40,23 +74,28 @@ namespace ut {
 
     struct {
         std::unique_ptr<pn532::esp32::hsu_channel> channel;
-        bool did_pass_wake_test = false;
         std::unique_ptr<pn532::controller> controller;
         bool did_pass_controller_test = false;
+        member_token::id_t nfc_id{};
         std::unique_ptr<desfire::tag> tag;
     } instance{};
 
     void test_wake_channel() {
         TEST_ASSERT(instance.channel != nullptr);
+        TEST_ASSERT(instance.controller != nullptr);
         if (instance.channel != nullptr) {
-            TEST_ASSERT(passthru_set(instance.did_pass_wake_test, instance.channel->wake()));
+            TEST_ASSERT(instance.channel->wake());
+            if (instance.controller != nullptr) {
+                TEST_ASSERT(instance.controller->sam_configuration(pn532::sam_mode::normal, 1s));
+            }
         }
     }
 
     void test_controller() {
         TEST_ASSERT(instance.controller != nullptr);
         if (instance.controller != nullptr) {
-            TEST_ASSERT(passthru_set(instance.did_pass_controller_test, instance.controller->diagnose_comm_line()));
+            TEST_ASSERT(passthru_set(instance.did_pass_controller_test,
+                                     instance.controller->diagnose_comm_line()));
             TEST_ASSERT(passthru_and(instance.did_pass_controller_test,
                                      instance.controller->diagnose_self_antenna(pn532::low_current_thr::mA_25, pn532::high_current_thr::mA_150)));
         }
@@ -68,8 +107,9 @@ namespace ut {
             return;
         }
         const desfire::any_key default_k{desfire::cipher_type::des};
-        const std::array<desfire::any_key, 8> keys_to_test{
+        const std::array<desfire::any_key, 9> keys_to_test{
                 default_k,
+                member_token::get_default_root_key(instance.nfc_id),
                 desfire::any_key{desfire::cipher_type::des3_2k},
                 desfire::any_key{desfire::cipher_type::des3_3k},
                 desfire::any_key{desfire::cipher_type::aes128},
@@ -106,36 +146,44 @@ namespace ut {
         TEST_ASSERT(instance.tag->format_picc());
     }
 
-    void test_encrypt_decrypt() {
-        const mlab::bin_data message = mlab::bin_data::chain(plaintext);
-        mlab::bin_data buffer = message;
+    void test_mad() {
+        TEST_ASSERT(instance.tag != nullptr);
+        if (instance.tag == nullptr) {
+            return;
+        }
 
-        key_pair k1, k2;
-        k1.generate();
-        k2.generate();
+        member_token token{*instance.tag};
+        TEST_ASSERT(token.setup_root_settings());
+        TEST_ASSERT(token.setup_mad("user", "Mittelab"));
+        TEST_ASSERT(token.unlock());
 
-        TEST_ASSERT(k1.is_valid());
-        TEST_ASSERT(k2.is_valid());
+        // Mad must be readable without auth
+        TEST_ASSERT(instance.tag->select_application());
+        TEST_ASSERT_FALSE(instance.tag->get_card_uid());
+        TEST_ASSERT(instance.tag->select_application());
 
-        TEST_ASSERT(k1.encrypt_for(k2, buffer));
-        TEST_ASSERT(k2.decrypt_from(k1, buffer));
+        const auto r_mad_version = token.get_mad_version();
+        const auto r_holder = token.get_holder();
+        const auto r_publisher = token.get_publisher();
 
-        TEST_ASSERT_EQUAL(buffer.size(), message.size());
-        TEST_ASSERT_EQUAL_HEX8_ARRAY(message.data(), buffer.data(), std::min(message.size(), buffer.size()));
+        TEST_ASSERT(r_mad_version);
+        TEST_ASSERT(r_holder);
+        TEST_ASSERT(r_publisher);
+
+        if (r_mad_version) {
+            TEST_ASSERT_EQUAL(*r_mad_version, member_token::mad_file_version);
+        }
+        if (r_holder) {
+            TEST_ASSERT(*r_holder == test_holder);
+        }
+        if (r_publisher) {
+            TEST_ASSERT(*r_publisher == test_publisher);
+        }
+
+        // Check that the mad application is not trivially writable
+        TEST_ASSERT(instance.tag->select_application(member_token::mad_aid));
+        TEST_ASSERT_FALSE(instance.tag->create_file(0x00, desfire::file_settings<desfire::file_type::value>{}));
     }
-
-    void test_keys() {
-        key_pair k;
-        TEST_ASSERT(not k.is_valid());
-
-        k.generate();
-        TEST_ASSERT(k.is_valid());
-
-        key_pair const k2{k.raw_sk()};
-        TEST_ASSERT(k2.is_valid());
-        TEST_ASSERT(k.raw_pk() == k2.raw_pk());
-    }
-
 }
 
 extern "C" void app_main() {
@@ -148,13 +196,9 @@ extern "C" void app_main() {
 
     ut::instance.channel = std::make_unique<pn532::esp32::hsu_channel>(
             ut::pinout::uart_port, ut::pinout::uart_config, ut::pinout::pn532_hsu_tx, ut::pinout::pn532_hsu_rx);
+    ut::instance.controller = std::make_unique<pn532::controller>(*ut::instance.channel);
 
     RUN_TEST(ut::test_wake_channel);
-
-    if (ut::instance.did_pass_wake_test) {
-        ut::instance.controller = std::make_unique<pn532::controller>(*ut::instance.channel);
-    }
-
     RUN_TEST(ut::test_controller);
 
     if (ut::instance.did_pass_controller_test and ut::instance.controller != nullptr) {
@@ -165,6 +209,7 @@ extern "C" void app_main() {
             for (auto const &target : *r_scan) {
                 ESP_LOGI("TEST", "Logical index %u; NFC ID:", target.logical_index);
                 ESP_LOG_BUFFER_HEX_LEVEL("TEST", target.info.nfcid.data(), target.info.nfcid.size(), ESP_LOG_INFO);
+                std::copy_n(std::begin(target.info.nfcid), ut::instance.nfc_id.size(), std::begin(ut::instance.nfc_id));
                 ut::instance.tag = std::make_unique<desfire::tag>(
                         desfire::tag::make<desfire::esp32::default_cipher_provider>(
                                 pn532::desfire_pcd{*ut::instance.controller, target.logical_index}));
@@ -179,6 +224,7 @@ extern "C" void app_main() {
 
         RUN_TEST(ut::test_tag_reset_root_key);
         RUN_TEST(ut::test_tag_format);
+        RUN_TEST(ut::test_mad);
 
     }
 
@@ -191,8 +237,10 @@ namespace ut {
     [[nodiscard]] bool passthru_set(bool &dest, Result const &res) {
         if constexpr (std::is_same_v<Result, bool>) {
             dest = res;
-        } else {
+        } else if constexpr(std::is_same_v<decltype(*res), bool>) {
             dest = res and *res;
+        } else {
+            dest = bool(res);
         }
         return dest;
     }
@@ -201,8 +249,10 @@ namespace ut {
     [[nodiscard]] bool passthru_and(bool &dest, Result const &res) {
         if constexpr (std::is_same_v<Result, bool>) {
             dest &= res;
-        } else {
+        } else if constexpr(std::is_same_v<decltype(*res), bool>) {
             dest &= res and *res;
+        } else {
+            dest &= bool(res);
         }
         return dest;
     }
