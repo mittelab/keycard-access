62b09236a60eba99c355b6e082a494589c8271c5
Aljaž "g5pw" Srebrnič <me@g5pw.me>
2023-01-18 23:18:32 +0100

    refactor: move id_from_nfc_id to data
    We'll reuse that function in cutter, too.

--- keycard-access/lib/libka/src/ka/data.cpp
+++ keycard-access/lib/libka/src/ka/data.cpp
@@ -47,6 +47,15 @@ namespace ka {
             }
             return retval;
         }
+
+        [[nodiscard]] token_id id_from_nfc_id(std::vector<std::uint8_t> const &d) {
+            if (d.size() != token_id::array_size) {
+                ESP_LOGE("KA", "NFC ID should be %d bytes long, not %d.", token_id::array_size, d.size());
+            }
+            token_id id{};
+            std::copy_n(std::begin(d), std::min(token_id::array_size, d.size()), std::begin(id));
+            return id;
+        }
     }
 
     std::string identity::string_representation() const {
