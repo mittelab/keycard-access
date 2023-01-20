55d8b1597e390a86044f49e54fb9f521051aa5ac
Pietro Saccardi <lizardm4@gmail.com>
2023-01-20 19:34:34 +0100

    Implement P2P gate configuration (gate side)

--- keycard-access/lib/libka/include/ka/p2p_ops.hpp
+++ keycard-access/lib/libka/include/ka/p2p_ops.hpp
@@ -5,4 +5,14 @@
 #ifndef KEYCARD_ACCESS_P2P_OPS_HPP
 #define KEYCARD_ACCESS_P2P_OPS_HPP
 
+#include <pn532/p2p.hpp>
+
+namespace ka {
+    class gate;
+}
+namespace ka::p2p {
+
+    void configure_gate(pn532::controller &ctrl, gate &g);
+}
+
 #endif//KEYCARD_ACCESS_P2P_OPS_HPP
--- keycard-access/lib/libka/src/ka/p2p_ops.cpp
+++ keycard-access/lib/libka/src/ka/p2p_ops.cpp
@@ -2,4 +2,71 @@
 // Created by spak on 1/20/23.
 //
 
+#include <desfire/esp32/utils.hpp>
+#include <ka/gate.hpp>
 #include <ka/p2p_ops.hpp>
+#include <ka/secure_p2p.hpp>
+#include <pn532/controller.hpp>
+#include <pn532/p2p.hpp>
+#include <ka/desfire_fs.hpp>
+
+
+namespace ka::p2p {
+    namespace bits {
+        static constexpr std::uint8_t command_code_configure = 0xcf;
+    }
+
+    namespace {
+        [[nodiscard]] std::array<std::uint8_t, 10> fabricate_nfcid(gate const &g) {
+            return {
+                    std::uint8_t(g.id() & 0xff),
+                    std::uint8_t((g.id() >> 8) & 0xff),
+                    std::uint8_t((g.id() >> 16) & 0xff),
+                    std::uint8_t((g.id() >> 24) & 0xff),
+                    0x6a, 0x7e, 0xde, 0xad, 0xbe, 0xef /* L33T garbage */
+            };
+        }
+
+        pn532::p2p::result<> configure_gate_internal(gate &g, secure_target &comm) {
+            TRY(comm.handshake());
+            ESP_LOGI("KA", "Comm opened, peer's public key:");
+            ESP_LOG_BUFFER_HEX_LEVEL("KA", comm.peer_pub_key().data(), comm.peer_pub_key().size(), ESP_LOG_INFO);
+
+            TRY_RESULT(comm.receive(1s)) {
+                gate_id new_id = g.id();
+                mlab::bin_stream s{*r};
+                if (s.pop() != bits::command_code_configure) {
+                    s.set_bad();
+                }
+                s >> mlab::lsb32 >> new_id;
+                std::string new_desc = mlab::data_to_string(s.peek());
+                if (s.bad()) {
+                    ESP_LOGE("KA", "Invalid configure command received.");
+                    return pn532::channel::error::comm_malformed;
+                }
+
+                // Finally:
+                g.configure(new_id, std::move(new_desc), pub_key{comm.peer_pub_key()});
+                TRY(comm.send(mlab::data_from_string("ok"), 1s))
+            }
+            return mlab::result_success;
+        }
+    }
+
+    void configure_gate(pn532::controller &ctrl, gate &g) {
+        pn532::p2p::pn532_target raw_comm{ctrl};
+        while (not g.is_configured()) {
+            // Make sure you get fresh new keys
+            g.regenerate_keys();
+            desfire::esp32::suppress_log suppress{ESP_LOG_ERROR, {PN532_TAG}};
+            while (not raw_comm.init_as_dep_target(fabricate_nfcid(g))) {
+                suppress.restore();
+                secure_target comm{raw_comm, g.keys()};
+                if (configure_gate_internal(g, comm)) {
+                    return;
+                }
+            }
+        }
+    }
+
+}
