da28f020ef29738de02ea7a1affe4fd63d510f93
Pietro Saccardi <lizardm4@gmail.com>
2023-01-21 19:38:05 +0100

    Bugfix, send base key

--- keycard-access/lib/libka/include/ka/p2p_ops.hpp
+++ keycard-access/lib/libka/include/ka/p2p_ops.hpp
@@ -14,24 +14,31 @@ namespace ka {
 
     struct keymaker_mock {
         key_pair _kp{randomize};
+        std::vector<gate_config> _gates;
 
         [[nodiscard]] key_pair const &keys() const { return _kp; }
-        [[nodiscard]] gate_id allocate_gate_id() { return 0xf00ba2; }
-        void register_gate([[maybe_unused]] gate_id gid, [[maybe_unused]] raw_pub_key gate_pk, [[maybe_unused]] std::string description) {}
+        [[nodiscard]] gate_id allocate_gate_id() { return _gates.size(); }
+        void register_gate(gate_config cfg) {
+            if (cfg.id == _gates.size()) {
+                _gates.emplace_back(cfg);
+            } else {
+                ESP_LOGE("KA", "Invalid gate.");
+            }
+        }
     };
 
     using keymaker = keymaker_mock;
 }
 namespace ka::p2p {
 
-    pn532::p2p::result<> configure_gate_exchange(keymaker &km, secure_initiator &comm, std::string gate_description);
+    pn532::p2p::result<> configure_gate_exchange(keymaker &km, secure_initiator &comm, std::string const &gate_description);
     pn532::p2p::result<> configure_gate_exchange(gate &g, secure_target &comm);
 
     [[nodiscard]] bool configure_gate_in_rf(pn532::controller &ctrl, gate &g);
-    [[nodiscard]] bool configure_gate_in_rf(pn532::controller &ctrl, std::uint8_t logical_index, keymaker &km, std::string gate_description);
+    [[nodiscard]] bool configure_gate_in_rf(pn532::controller &ctrl, std::uint8_t logical_index, keymaker &km, std::string const &gate_description);
 
     void configure_gate_loop(pn532::controller &ctrl, gate &g);
-    [[nodiscard]] bool configure_gate_loop(pn532::controller &ctrl, keymaker &km, std::string gate_description);
+    [[nodiscard]] bool configure_gate_loop(pn532::controller &ctrl, keymaker &km, std::string const &gate_description);
 
 }
 
--- keycard-access/lib/libka/src/ka/p2p_ops.cpp
+++ keycard-access/lib/libka/src/ka/p2p_ops.cpp
@@ -14,7 +14,6 @@
 namespace ka::p2p {
     namespace bits {
         static constexpr std::uint8_t command_code_configure = 0xcf;
-        static constexpr auto command_ok = "ok";
     }
 
     namespace {
@@ -31,10 +30,10 @@ namespace ka::p2p {
 
         class configure_gate_responder final : public pn532::scanner_responder {
             keymaker &_km;
-            std::string _desc;
+            std::string const &_desc;
             bool _success;
         public:
-            configure_gate_responder(keymaker &km, std::string desc) : _km{km}, _desc{std::move(desc)}, _success{false} {}
+            configure_gate_responder(keymaker &km, std::string const &desc) : _km{km}, _desc{desc}, _success{false} {}
 
             [[nodiscard]] inline bool success() const { return _success; }
 
@@ -67,8 +66,8 @@ namespace ka::p2p {
         }
     }
 
-    bool configure_gate_loop(pn532::controller &ctrl, keymaker &km, std::string gate_description) {
-        configure_gate_responder responder{km, std::move(gate_description)};
+    bool configure_gate_loop(pn532::controller &ctrl, keymaker &km, std::string const &gate_description) {
+        configure_gate_responder responder{km, gate_description};
         pn532::scanner scanner{ctrl};
         scanner.loop(responder, false);
         return responder.success();
@@ -94,12 +93,12 @@ namespace ka::p2p {
 
             // Finally:
             g.configure(new_id, std::move(new_desc), pub_key{comm.peer_pub_key()});
-            TRY(comm.send(mlab::data_from_string(bits::command_ok), 1s))
+            TRY(comm.send(mlab::bin_data::chain(g.app_base_key()), 1s))
         }
         return mlab::result_success;
     }
 
-    pn532::p2p::result<> configure_gate_exchange(keymaker &km, secure_initiator &comm, std::string gate_description) {
+    pn532::p2p::result<> configure_gate_exchange(keymaker &km, secure_initiator &comm, std::string const &gate_description) {
         TRY(comm.handshake());
         ESP_LOGI("KA", "Comm opened, peer's public key:");
         ESP_LOG_BUFFER_HEX_LEVEL("KA", comm.peer_pub_key().data(), comm.peer_pub_key().size(), ESP_LOG_INFO);
@@ -111,14 +110,15 @@ namespace ka::p2p {
         mlab::bin_data msg{mlab::prealloc(6 + gate_description.size())};
         msg << bits::command_code_configure << mlab::lsb32 << gid << mlab::view_from_string(gate_description);
         TRY_RESULT(comm.communicate(msg, 1s)) {
-            const std::string response = mlab::data_to_string(*r);
-            if (response != bits::command_ok) {
+            if (r->size() != gate_app_base_key::array_size) {
                 ESP_LOGE("KA", "Invalid configure response received.");
                 return pn532::channel::error::comm_malformed;
             }
+            gate_app_base_key base_key{};
+            std::copy(std::begin(*r), std::end(*r), std::begin(base_key));
+            km.register_gate({gid, pub_key{comm.peer_pub_key()}, base_key});
         }
 
-        km.register_gate(gid, comm.peer_pub_key(), std::move(gate_description));
         return mlab::result_success;
     }
 
@@ -128,9 +128,9 @@ namespace ka::p2p {
         return bool(configure_gate_exchange(g, dynamic_cast<secure_target &>(comm)));
     }
 
-    bool configure_gate_in_rf(pn532::controller &ctrl, std::uint8_t logical_index, keymaker &km, std::string gate_description) {
+    bool configure_gate_in_rf(pn532::controller &ctrl, std::uint8_t logical_index, keymaker &km, std::string const &gate_description) {
         pn532::p2p::pn532_initiator raw_comm{ctrl, logical_index};
         secure_initiator comm{raw_comm, km.keys()};
-        return bool(configure_gate_exchange(km, comm, std::move(gate_description)));
+        return bool(configure_gate_exchange(km, comm, gate_description));
     }
 }
