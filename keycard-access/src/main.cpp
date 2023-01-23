26990232e9d16a4a3343c74986b4afed546a226d
Pietro Saccardi <lizardm4@gmail.com>
2023-01-24 00:40:49 +0100

    Change log prefixes so that error messages do not get suppressed

--- keycard-access/src/main.cpp
+++ keycard-access/src/main.cpp
@@ -7,7 +7,12 @@
 #include <ka/p2p_ops.hpp>
 #include <pn532/controller.hpp>
 #include <pn532/esp32/hsu.hpp>
-#include <ka/desfire_fs.hpp>
+
+
+// Override the log prefix
+#define LOG_PFX "KADEMO"
+#define DESFIRE_FS_LOG_PREFIX LOG_PFX
+#include <desfire/fs.hpp>
 
 using namespace std::chrono_literals;
 
@@ -16,28 +21,28 @@ void gate_main() {
     pn532::controller controller{hsu_chn};
     pn532::scanner scanner{controller};
 
-    ESP_LOGI("KA", "Loading configuration.");
+    ESP_LOGI(LOG_PFX, "Loading configuration.");
     ka::gate gate = ka::gate::load_from_config();
 
     if (not scanner.init_and_test_controller()) {
-        ESP_LOGE("KA", "Power cycle the device to try again.");
+        ESP_LOGE(LOG_PFX, "Power cycle the device to try again.");
         return;
     }
 
-    ESP_LOGI("KA", "Self-test passed.");
+    ESP_LOGI(LOG_PFX, "Self-test passed.");
 
     if (not gate.is_configured()) {
-        ESP_LOGW("KA", "Gate is not configured, entering target mode.");
+        ESP_LOGW(LOG_PFX, "Gate is not configured, entering target mode.");
         ka::p2p::configure_gate_loop(controller, gate);
         gate.config_store();
-        ESP_LOGI("KA", "Gate configured.");
+        ESP_LOGI(LOG_PFX, "Gate configured.");
     }
 
-    ESP_LOGI("KA", "Gate %d \"%s\".", gate.id(), gate.description().c_str());
-    ESP_LOGI("KA", "Gate public key:");
-    ESP_LOG_BUFFER_HEX_LEVEL("KA", gate.keys().raw_pk().data(), ka::raw_pub_key::array_size, ESP_LOG_INFO);
-    ESP_LOGI("KA", "Keymaker public key:");
-    ESP_LOG_BUFFER_HEX_LEVEL("KA", gate.programmer_pub_key().raw_pk().data(), ka::raw_pub_key::array_size, ESP_LOG_INFO);
+    ESP_LOGI(LOG_PFX, "Gate %d \"%s\".", gate.id(), gate.description().c_str());
+    ESP_LOGI(LOG_PFX, "Gate public key:");
+    ESP_LOG_BUFFER_HEX_LEVEL(LOG_PFX, gate.keys().raw_pk().data(), ka::raw_pub_key::array_size, ESP_LOG_INFO);
+    ESP_LOGI(LOG_PFX, "Keymaker public key:");
+    ESP_LOG_BUFFER_HEX_LEVEL(LOG_PFX, gate.programmer_pub_key().raw_pk().data(), ka::raw_pub_key::array_size, ESP_LOG_INFO);
 
     ka::gate_responder responder{gate};
     scanner.loop(responder, false /* already performed */);
@@ -46,22 +51,18 @@ void gate_main() {
 struct format_mcformatface final : public desfire::tag_responder<desfire::esp32::default_cipher_provider> {
     ka::token_id current_id{};
 
-    void get_scan_target_types(pn532::scanner &, std::vector<pn532::target_type> &targets) const override {
-        targets = pn532::controller::poll_all_targets;
-    }
-
     void on_activation(pn532::scanner &, pn532::scanned_target const &target) override {
-        const auto s_id = ka::util::hex_string(mlab::make_range(target.nfcid.data(), target.nfcid.data() + target.nfcid.size()));
+        const auto s_id = ka::util::hex_string(target.nfcid);
         ESP_LOGI("NFC", "Activated: %s target %s.", pn532::to_string(target.type), s_id.c_str());
     }
 
     void on_release(pn532::scanner &, pn532::scanned_target const &target) override {
-        const auto s_id = ka::util::hex_string(mlab::make_range(target.nfcid.data(), target.nfcid.data() + target.nfcid.size()));
+        const auto s_id = ka::util::hex_string(target.nfcid);
         ESP_LOGI("NFC", "Released: %s target %s.", pn532::to_string(target.type), s_id.c_str());
     }
 
     void on_leaving_rf(pn532::scanner &, pn532::scanned_target const &target) override {
-        const auto s_id = ka::util::hex_string(mlab::make_range(target.nfcid.data(), target.nfcid.data() + target.nfcid.size()));
+        const auto s_id = ka::util::hex_string(target.nfcid);
         ESP_LOGI("NFC", "Out of RF: %s target %s.", pn532::to_string(target.type), s_id.c_str());
     }
 
@@ -107,43 +108,49 @@ struct format_mcformatface final : public desfire::tag_responder<desfire::esp32:
                     desfire::any_key{desfire::cipher_type::des3_3k, mlab::make_range(secondary_des3_3k_key), 0, secondary_keys_version},
                     desfire::any_key{desfire::cipher_type::aes128, mlab::make_range(secondary_aes_key), 0, secondary_keys_version}};
         const auto s_nfcid = ka::util::hex_string(current_id);
-        ESP_LOGI("KA", "Attempting to recover root key for ID %s", s_nfcid.c_str());
+        ESP_LOGI(LOG_PFX, "Attempting to recover root key for ID %s", s_nfcid.c_str());
         TRY(tag.select_application());
         for (auto const &key : keys_to_test) {
+            const auto key_body = key.get_packed_key_body();
+            const auto s_key_body = ka::util::hex_string(key_body);
+            ESP_LOGI(LOG_PFX, "Trying %s...", s_key_body.c_str());
             auto suppress = desfire::esp32::suppress_log{DESFIRE_LOG_PREFIX};
             if (tag.authenticate(key)) {
                 suppress.restore();
-                ESP_LOGI("KA", "Found the right key, changing to default.");
+                ESP_LOGI(LOG_PFX, "Found the right key, changing to default.");
                 TRY(tag.change_key(default_k));
                 TRY(tag.authenticate(default_k));
-                ESP_LOGI("KA", "NFC ID: %s", s_nfcid.c_str());
+                ESP_LOGI(LOG_PFX, "NFC ID: %s", s_nfcid.c_str());
                 TRY_RESULT(tag.get_info()) {
                     const auto s_serial = ka::util::hex_string(r->serial_no);
-                    ESP_LOGI("KA", "Serial: %s", s_nfcid.c_str());
+                    ESP_LOGI(LOG_PFX, "Serial: %s", s_nfcid.c_str());
                 }
                 TRY_RESULT(tag.get_card_uid()) {
                     const auto s_card_uid = ka::util::hex_string(*r);
-                    ESP_LOGI("KA", "CardID: %s", s_nfcid.c_str());
+                    ESP_LOGI(LOG_PFX, "CardID: %s", s_nfcid.c_str());
                 }
                 TRY_RESULT(tag.get_application_ids()) {
-                    ESP_LOGI("KA", "  Apps: %d", r->size());
-                    for (std::size_t i = 0; i < r->size(); ++i) {
-                        ESP_LOGI("KA", "       %2d. %02x%2x%2x", i + 1, (*r)[i][0], (*r)[i][1], (*r)[i][2]);
+                    if (r->empty()) {
+                        ESP_LOGI(LOG_PFX, "  Apps: none");
+                    } else {
+                        for (std::size_t i = 0; i < r->size(); ++i) {
+                            ESP_LOGI(LOG_PFX, "  %s %2d. %02x%2x%2x", (i == 0 ? "Apps:" : "     "), i + 1, (*r)[i][0], (*r)[i][1], (*r)[i][2]);
+                        }
                     }
                 }
                 TRY(tag.format_picc());
-                ESP_LOGI("KA", "Formatted.");
+                ESP_LOGI(LOG_PFX, "Formatted.");
                 return mlab::result_success;
             }
         }
-        ESP_LOGE("KA", "I do not know the key...");
+        ESP_LOGE(LOG_PFX, "I do not know the key...");
         return mlab::result_success;
     }
 
     pn532::post_interaction interact_with_tag(desfire::tag &tag) override {
-        ESP_LOGI("KA", "Beginning interaction.");
+        ESP_LOGI(LOG_PFX, "Beginning interaction.");
         const auto success = bool(interact_with_tag_internal(tag));
-        ESP_LOG_LEVEL_LOCAL((success ? ESP_LOG_INFO : ESP_LOG_WARN), "KA", "Interaction complete.");
+        ESP_LOG_LEVEL_LOCAL((success ? ESP_LOG_INFO : ESP_LOG_WARN), LOG_PFX, "Interaction complete.");
         return pn532::post_interaction::reject;
     }
 };
@@ -163,83 +170,80 @@ struct keymaker_responder final : public ka::member_token_responder {
     }
 
     pn532::post_interaction interact(pn532::scanner &scanner, pn532::scanned_target const &target) override {
-        const auto s_nfcid = ka::util::hex_string(mlab::make_range(target.nfcid.data(), target.nfcid.data() + target.nfcid.size()));
-        ESP_LOGI("KA", "Found %s target with NFC ID %s.", pn532::to_string(target.type), s_nfcid.c_str());
+        const auto s_nfcid = ka::util::hex_string(target.nfcid);
+        ESP_LOGI(LOG_PFX, "Found %s target with NFC ID %s.", pn532::to_string(target.type), s_nfcid.c_str());
         if (target.type == pn532::target_type::passive_106kbps_iso_iec_14443_4_typea) {
             return desfire::tag_responder<desfire::esp32::default_cipher_provider>::interact(scanner, target);
         } else {
             // Enter a gate configuration loop
             if (ka::p2p::configure_gate_in_rf(scanner.ctrl(), target.index, km, "Dummy gate")) {
-                ESP_LOGI("KA", "Gate configured.");
+                ESP_LOGI(LOG_PFX, "Gate configured.");
             } else {
-                ESP_LOGE("KA", "Gate not configured.");
+                ESP_LOGE(LOG_PFX, "Gate not configured.");
             }
         }
         return pn532::post_interaction::reject;
     }
 
     desfire::tag::result<> interact_with_token_internal(ka::member_token &token) {
-        desfire::esp32::suppress_log suppress{"KA", DESFIRE_FS_DEFAULT_LOG_PREFIX, DESFIRE_DEFAULT_LOG_PREFIX};
+        desfire::esp32::suppress_log suppress{LOG_PFX, DESFIRE_FS_DEFAULT_LOG_PREFIX, DESFIRE_DEFAULT_LOG_PREFIX};
         TRY_RESULT_AS(token.get_id(), r_id) {
             const auto root_key = km.keys().derive_token_root_key(*r_id);
             suppress.restore();
             const std::string id_str = ka::util::hex_string(*r_id);
-            ESP_LOGI("KA", "Got the following token: %s.", id_str.c_str());
+            ESP_LOGI(LOG_PFX, "Got the following token: %s.", id_str.c_str());
             suppress.suppress();
             if (token.unlock_root()) {
                 suppress.restore();
-                ESP_LOGI("KA", "Empty token, setting up MAD.");
+                ESP_LOGI(LOG_PFX, "Empty token, setting up MAD.");
                 TRY(token.tag().format_picc())
                 TRY(token.setup_root(root_key))
                 TRY(token.setup_mad(ka::identity{*r_id, "Holder", "Publisher"}))
             } else if (token.try_set_root_key(root_key)) {
                 suppress.restore();
-                ESP_LOGI("KA", "Token was set up.");
+                ESP_LOGI(LOG_PFX, "Token was set up.");
                 suppress.suppress();
                 if (const auto r_identity = token.get_identity(); r_identity) {
                     suppress.restore();
-                    ESP_LOGI("KA", "MAD was set up for user %s (%s).", r_identity->holder.c_str(), r_identity->publisher.c_str());
-                    ESP_LOGW("KA", "We are now trusting this data, during regular operation, it should be authenticated!");
-                    ESP_LOGI("KA", "Setting up gates.");
+                    ESP_LOGI(LOG_PFX, "MAD was set up for user %s (%s).", r_identity->holder.c_str(), r_identity->publisher.c_str());
+                    ESP_LOGW(LOG_PFX, "We are now trusting this data, during regular operation, it should be authenticated!");
+                    ESP_LOGI(LOG_PFX, "Setting up gates.");
                     bool all_enrolled = true;
                     for (ka::gate_config const &cfg : km._gates) {
                         if (token.is_gate_enrolled(cfg.id)) {
-                            ESP_LOGI("KA", "Gate %d was already enrolled.", cfg.id);
+                            ESP_LOGI(LOG_PFX, "Gate %d was already enrolled.", cfg.id);
                         } else {
                             all_enrolled = false;
                             TRY(token.enroll_gate(cfg.id, cfg.app_base_key.derive_app_master_key(*r_id), *r_identity))
-                            ESP_LOGI("KA", "I just enrolled gate %d", cfg.id);
+                            ESP_LOGI(LOG_PFX, "I just enrolled gate %d", cfg.id);
                         }
                     }
                     if (all_enrolled) {
-                        ESP_LOGI("KA", "All gates enrolled, I'll format this PICC.");
+                        ESP_LOGI(LOG_PFX, "All gates enrolled, I'll format this PICC.");
                         TRY(token.unlock_root())
-                        /**
-                         * @todo Formatting seems to have no effect, is the whole thing running correctly?
-                         */
                         TRY(token.tag().format_picc())
                         TRY(token.unlock_root())
                         TRY(token.tag().change_key(desfire::key<desfire::cipher_type::des>{}))
                     }
                 } else {
                     suppress.restore();
-                    ESP_LOGW("KA", "MAD was not set up, will format the PICC.");
+                    ESP_LOGW(LOG_PFX, "MAD was not set up, will format the PICC.");
                     TRY(token.unlock_root())
-                    /**
-                     * @todo Formatting seems to have no effect, is the whole thing running correctly?
-                     */
                     TRY(token.tag().format_picc())
                     TRY(token.unlock_root())
                     TRY(token.tag().change_key(desfire::key<desfire::cipher_type::des>{}))
                 }
+            } else {
+                ESP_LOGE(LOG_PFX, "I do not know the key of this PICC.");
+                return desfire::error::permission_denied;
             }
         }
         return mlab::result_success;
     }
 
     pn532::post_interaction interact_with_token(ka::member_token &token) override {
-        interact_with_token_internal(token);
-        ESP_LOGI("KA", "Interaction complete.");
+        const bool success = bool(interact_with_token_internal(token));
+        ESP_LOG_LEVEL_LOCAL((success ? ESP_LOG_INFO : ESP_LOG_ERROR), LOG_PFX, "Interaction complete.");
         return pn532::post_interaction::reject;
     }
 };
@@ -252,11 +256,11 @@ void keymaker_main() {
     keymaker_responder responder{};
 
     if (not scanner.init_and_test_controller()) {
-        ESP_LOGE("KA", "Power cycle the device to try again.");
+        ESP_LOGE(LOG_PFX, "Power cycle the device to try again.");
         return;
     }
 
-    ESP_LOGI("KA", "Self-test passed.");
+    ESP_LOGI(LOG_PFX, "Self-test passed.");
     scanner.loop(responder, false /* already performed */);
 }
 
@@ -268,17 +272,17 @@ void format_mcformatface_main() {
     format_mcformatface responder{};
 
     if (not scanner.init_and_test_controller()) {
-        ESP_LOGE("KA", "Power cycle the device to try again.");
+        ESP_LOGE(LOG_PFX, "Power cycle the device to try again.");
         return;
     }
 
-    ESP_LOGI("KA", "Self-test passed.");
+    ESP_LOGI(LOG_PFX, "Self-test passed.");
     scanner.loop(responder, false /* already performed */);
 }
 
 extern "C" void app_main() {
     desfire::esp32::suppress_log suppress{"AUTH ROOT KEY"};
-    ESP_LOGI("KA", "Waiting 2s to ensure the serial is attached and visible...");
+    ESP_LOGI(LOG_PFX, "Waiting 2s to ensure the serial is attached and visible...");
     vTaskDelay(pdMS_TO_TICKS(2000));
     int choice = 0;
     while (choice == 0) {
