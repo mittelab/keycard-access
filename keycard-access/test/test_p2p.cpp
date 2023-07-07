b695f55833c0fea970651b1531c7770c95485d00
Pietro Saccardi <lizardm4@gmail.com>
2023-07-07 14:51:25 +0200

    Fix race conditions

--- keycard-access/test/test_p2p.cpp
+++ keycard-access/test/test_p2p.cpp
@@ -19,34 +19,29 @@ namespace ut {
         using ms = std::chrono::milliseconds;
 
         struct p2p_loopback : public pn532::p2p::initiator, public pn532::p2p::target {
-            mlab::bin_data data{};
-            std::condition_variable i2t_ready{};
-            std::mutex i2t_ready_mutex{};
-            std::condition_variable t2i_ready{};
-            std::mutex t2i_ready_mutex{};
+            mlab::bin_data i2t_data, t2i_data;
+            std::binary_semaphore i2t_avail{0}, t2i_avail{0};
 
             pn532::result<mlab::bin_data> receive(ms timeout) override {
-                std::unique_lock<std::mutex> lock{i2t_ready_mutex};
-                if (i2t_ready.wait_for(lock, timeout) == std::cv_status::no_timeout) {
-                    return data;
+                if (not i2t_avail.try_acquire_for(timeout)) {
+                    return pn532::channel_error::timeout;
                 }
-                return pn532::channel_error::timeout;
+                return i2t_data;
             }
 
             pn532::result<> send(mlab::bin_data const &data_to_send, ms timeout) override {
-                data = data_to_send;
-                t2i_ready.notify_one();
+                t2i_data = data_to_send;
+                t2i_avail.release();
                 return mlab::result_success;
             }
 
             pn532::result<mlab::bin_data> communicate(mlab::bin_data const &data_to_send, ms timeout) override {
-                std::unique_lock<std::mutex> lock{t2i_ready_mutex};
-                data = data_to_send;
-                i2t_ready.notify_one();
-                if (t2i_ready.wait_for(lock, timeout) == std::cv_status::no_timeout) {
-                    return data;
+                i2t_data = data_to_send;
+                i2t_avail.release();
+                if (not t2i_avail.try_acquire_for(timeout)) {
+                    return pn532::channel_error::timeout;
                 }
-                return pn532::channel_error::timeout;
+                return t2i_data;
             }
         };
 
@@ -58,9 +53,9 @@ namespace ut {
 
             loopback_bundle() {
                 std::thread t{[&]() {
-                    TEST_ASSERT(target.handshake(5s));
+                    TEST_ASSERT(initiator.handshake(5s));
                 }};
-                TEST_ASSERT(initiator.handshake(5s));
+                TEST_ASSERT(target.handshake(5s));
                 t.join();
             }
         };
@@ -111,65 +106,66 @@ namespace ut {
         assertive_local_gate lg{};
         ka::p2p::v0::remote_gate rg{lg.target};
 
-        auto trigger_test = [&]() {
-            {
-                ESP_LOGI("UT", "Testing %s", "hello");
-                auto r = rg.hello();
-                TEST_ASSERT(r);
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "get_update_settings");
-                auto r = rg.get_update_settings();
-                TEST_ASSERT(r);
-                if (r) {
-                    TEST_ASSERT(not r->enable_automatic_update);
-                    TEST_ASSERT(r->update_channel == "Foo bar");
-                }
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "set_update_settings");
-                auto r = rg.set_update_settings("The Channel", true);
-                TEST_ASSERT(r);
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "get_wifi_status");
-                auto r = rg.get_wifi_status();
-                TEST_ASSERT(r);
-                if (r) {
-                    TEST_ASSERT(r->ssid == "Nope");
-                    TEST_ASSERT(not r->operational);
-                }
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "connect_wifi");
-                auto r = rg.connect_wifi("Test SSID", "Test Password");
-                TEST_ASSERT(r);
-                if (r) {
-                    TEST_ASSERT(not *r);
-                }
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "get_registration_info");
-                auto r = rg.get_registration_info();
-                TEST_ASSERT(r);
-                if (r) {
-                    TEST_ASSERT(r->id == ka::gate_id{32});
-                    TEST_ASSERT(r->km_pk.raw_pk() == lg.g0_uncfg.keys().raw_pk());
-                }
-            }
-            {
-                ESP_LOGI("UT", "Testing %s", "register_gate");
-                auto r = rg.register_gate(ka::gate_id{13});
-                TEST_ASSERT(r);
-                if (r) {
-                    TEST_ASSERT(*r == lg.g0_bk);
-                }
-            }
-            rg.bye();
-        };
+        TEST_ASSERT(lg.initiator.did_handshake());
+        TEST_ASSERT(lg.target.did_handshake());
 
-        std::thread t{trigger_test};
-        lg.serve_loop();
+        std::thread t{[&]() { lg.serve_loop(); }};
+
+        {
+            ESP_LOGI("UT", "Testing %s", "hello");
+            auto r = rg.hello();
+            TEST_ASSERT(r);
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "get_update_settings");
+            auto r = rg.get_update_settings();
+            TEST_ASSERT(r);
+            if (r) {
+                TEST_ASSERT(not r->enable_automatic_update);
+                TEST_ASSERT(r->update_channel == "Foo bar");
+            }
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "set_update_settings");
+            auto r = rg.set_update_settings("The Channel", true);
+            TEST_ASSERT(r);
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "get_wifi_status");
+            auto r = rg.get_wifi_status();
+            TEST_ASSERT(r);
+            if (r) {
+                TEST_ASSERT(r->ssid == "Nope");
+                TEST_ASSERT(not r->operational);
+            }
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "connect_wifi");
+            auto r = rg.connect_wifi("Test SSID", "Test Password");
+            TEST_ASSERT(r);
+            if (r) {
+                TEST_ASSERT(not *r);
+            }
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "get_registration_info");
+            auto r = rg.get_registration_info();
+            TEST_ASSERT(r);
+            if (r) {
+                TEST_ASSERT(r->id == ka::gate_id{32});
+                TEST_ASSERT(r->km_pk.raw_pk() == lg.g0_uncfg.keys().raw_pk());
+            }
+        }
+        {
+            ESP_LOGI("UT", "Testing %s", "register_gate");
+            auto r = rg.register_gate(ka::gate_id{13});
+            TEST_ASSERT(r);
+            if (r) {
+                TEST_ASSERT(*r == lg.g0_bk);
+            }
+        }
+        rg.bye();
+
         t.join();
     }
 }// namespace ut
