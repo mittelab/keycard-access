b2036b89e9ab38ec6d4cca7e4fcf7c8e9f7d1558
Pietro Saccardi <lizardm4@gmail.com>
2023-07-21 00:06:19 +0200

    Add draft bridge system to replace current nfc p2p stuff

---
+++ keycard-access/lib/libka/include/ka/rpc.hpp
@@ -0,0 +1,366 @@
+//
+// Created by spak on 7/18/23.
+//
+
+#ifndef KEYCARD_ACCESS_RPC_HPP
+#define KEYCARD_ACCESS_RPC_HPP
+
+
+#include <mlab/bin_data.hpp>
+#include <mlab/result.hpp>
+#include <mlab/strutils.hpp>
+#include <ka/misc.hpp>
+#include <mlab/type_name.hpp>
+#include <type_traits>
+#include <utility>
+#include <map>
+
+namespace pn532 {
+    struct target;
+    struct initiator;
+}
+
+namespace ka::rpc {
+
+    template <class T>
+    struct use_default_serialization : std::false_type {};
+
+    template <class T>
+    struct serializer {
+        static void serialize(mlab::bin_data &bd, T const &t);
+        [[nodiscard]] static auto deserialize(mlab::bin_stream &s);
+
+        using deserialize_type_t = decltype(deserialize(std::declval<mlab::bin_stream &>()));
+    };
+
+    enum struct error : std::uint8_t {
+        parsing_error = 0,
+        no_handler,
+        unknown_command,
+        mismatching_signature,
+        transport_error,
+        channel_error,
+        invalid_argument,
+        app_specific_error
+    };
+
+    template <class ...Args>
+    using r = mlab::result<error, Args...>;
+
+    template <class ...Args>
+    using deserialized_args_tuple_t = std::tuple<typename serializer<Args>::deserialize_type_t...>;
+
+    template <class ...Args>
+    using serialized_args_tuple_t = std::tuple<Args...>;
+
+    template <class ...Args>
+    [[nodiscard]] std::optional<deserialized_args_tuple_t<Args...>> deserialize(mlab::bin_stream &s);
+
+    template <class ...Args>
+    [[nodiscard]] mlab::bin_data serialize(Args... args);
+
+    template <class R, class T, class... Args>
+    [[nodiscard]] std::string signature_of(R (T::*)(Args...));
+
+    template <class R, class T, class... Args>
+    [[nodiscard]] std::string signature_of(R (T::*)(Args...) const);
+
+    struct command_base {
+        std::string signature = {};
+
+        virtual ~command_base() = default;
+        explicit command_base(std::string signature_);
+
+        [[nodiscard]] virtual r<mlab::bin_data> command_response(mlab::bin_stream &args) = 0;
+    };
+
+    template <class R, class T, class... Args>
+    struct templated_command : command_base, ka::target_method<R, T, Args...> {
+        using typename ka::target_method<R, T, Args...>::fn_ptr_t;
+        using typename ka::target_method<R, T, Args...>::target_ptr_t;
+
+        explicit templated_command(fn_ptr_t method, target_ptr_t ptr = nullptr);
+
+        [[nodiscard]] r<mlab::bin_data> command_response(mlab::bin_stream &s) override;
+    private:
+        template <std::size_t ...Is>
+        [[nodiscard]] r<mlab::bin_data> invoke_with_tuple(std::index_sequence<Is...>, deserialized_args_tuple_t<Args...> t);
+    };
+
+    struct bridge_remote_interface {
+        virtual ~bridge_remote_interface() = default;
+
+        [[nodiscard]] virtual r<mlab::bin_data> command_response(mlab::bin_data const &cmd) = 0;
+    };
+
+    struct bridge_local_interface {
+        virtual ~bridge_local_interface() = default;
+
+        [[nodiscard]] virtual r<mlab::bin_data> receive() = 0;
+        [[nodiscard]] virtual r<> send(mlab::bin_data const &rsp) = 0;
+    };
+
+    class bridge {
+        std::map<std::string, std::unique_ptr<command_base>> _cmds;
+        std::unique_ptr<bridge_remote_interface> _remote_if;
+        std::unique_ptr<bridge_local_interface> _local_if;
+        bool _serve_stop = false;
+
+        [[nodiscard]] r<std::string_view> register_command(std::string uuid, std::unique_ptr<command_base> cmd);
+
+        [[nodiscard]] r<mlab::bin_data> remote_invoke(std::string_view uuid, mlab::bin_data const &body);
+
+        [[nodiscard]] r<mlab::bin_data> command_response(mlab::bin_data const &payload) const;
+
+        [[nodiscard]] r<mlab::bin_data> local_invoke(mlab::bin_data const &packed_cmd) const;
+
+        [[nodiscard]] r<mlab::bin_data> local_invoke(std::string_view uuid, mlab::bin_stream &s) const;
+    public:
+        bridge() = default;
+
+        explicit bridge(std::unique_ptr<bridge_remote_interface> remote_if, std::unique_ptr<bridge_local_interface> local_if);
+
+        bridge(bridge const &) = delete;
+        bridge &operator=(bridge const &) = delete;
+
+        bridge(bridge &&) = default;
+        bridge &operator=(bridge &&) = default;
+
+        template <class R, class T, class... Args>
+        r<std::string_view> register_command(R (T::*method)(Args...), std::string uuid = "", T *handler = nullptr);
+
+        template <class R, class T, class... Args>
+        r<std::string_view> register_command(R (T::*method)(Args...) const, std::string uuid = "", T const *handler = nullptr);
+
+        [[nodiscard]] bool contains(std::string_view uuid) const;
+
+        /**
+         * Will return @ref error::invalid_argument if multiple uuids match the signature.
+         * Will return @ref error::mismatching_signature if none is found.
+         */
+        [[nodiscard]] r<std::string_view> lookup_uuid(std::string_view signature) const;
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<std::string_view> lookup_uuid(R (T::*method)(Args...)) const;
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<std::string_view> lookup_uuid(R (T::*method)(Args...) const) const;
+
+        [[nodiscard]] std::string_view get_signature(std::string_view uuid) const;
+
+        [[nodiscard]] r<std::string> remote_get_signature(std::string_view uuid) const;
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<> remote_supports(R (T::*method)(Args...) const, std::string_view uuid = "") const;
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<> remote_supports(R (T::*method)(Args...), std::string_view uuid = "") const;
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<R> remote_invoke_unique(R (T::*method)(Args...) const, Args... args);
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<R> remote_invoke_unique(R (T::*method)(Args...), Args... args);
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<R> remote_invoke(R (T::*method)(Args...) const, std::string_view uuid, Args... args);
+
+        template <class R, class T, class... Args>
+        [[nodiscard]] r<R> remote_invoke(R (T::*method)(Args...), std::string_view uuid, Args... args);
+
+        r<> serve_loop();
+
+        void serve_stop();
+    };
+
+}
+
+namespace ka::rpc {
+
+
+    template <>
+    struct use_default_serialization<bool> : std::true_type {};
+
+    template <mlab::is_byte_enum T, std::size_t N>
+    struct use_default_serialization<std::array<T, N>> : std::true_type {};
+
+    template <std::size_t N>
+    struct use_default_serialization<std::array<std::uint8_t, N>> : std::true_type {};
+
+
+    template <class T>
+    void serializer<T>::serialize(mlab::bin_data &bd, T const &t) {
+        if constexpr (std::is_arithmetic_v<T>) {
+            bd << mlab::lsb_auto << t;
+        } else if constexpr (std::is_same_v<T, std::string_view> or std::is_same_v<T, std::string>) {
+            bd << mlab::length_encoded << t;
+        } else {
+            static_assert(use_default_serialization<T>::value, "I do not know how to encode this type!");
+            static_assert(mlab::is_injectable<T>, "You marked this type for using default serialization, but it's not injectable.");
+            bd << t;
+        }
+    }
+
+    template <class T>
+    auto serializer<T>::deserialize(mlab::bin_stream &s) {
+        if constexpr (std::is_arithmetic_v<T>) {
+            T retval{};
+            s >> mlab::lsb_auto >> retval;
+            return retval;
+        } else if constexpr (std::is_same_v<T, std::string_view> or std::is_same_v<T, std::string>) {
+            std::string retval{};
+            s >> mlab::length_encoded >> retval;
+            return retval;
+        } else {
+            static_assert(use_default_serialization<T>::value, "I do not know how to encode this type!");
+            static_assert(mlab::is_extractable<T>, "You marked this type for using default serialization, but it's not extractable.");
+            T retval{};
+            s >> retval;
+            return retval;
+        }
+    }
+
+    template <class ...Args>
+    std::optional<deserialized_args_tuple_t<Args...>> deserialize(mlab::bin_stream &s) {
+        auto retval = deserialized_args_tuple_t<Args...>{serializer<std::decay_t<Args>>::deserialize(s)...};
+        if (s.bad() or not s.eof()) {
+            return std::nullopt;
+        }
+        return retval;
+    }
+
+    template <class ...Args>
+    mlab::bin_data serialize(Args... args) {
+        mlab::bin_data bd;
+        (serializer<std::decay_t<Args>>::serialize(bd, args), ...);
+        return bd;
+    }
+
+    template <class R, class T, class... Args>
+    std::string signature_of(R (T::*)(Args...)) {
+        return mlab::type_name<R (T::*)(Args...)>();
+    }
+
+    template <class R, class T, class... Args>
+    std::string signature_of(R (T::*)(Args...) const) {
+        return mlab::type_name<R (T::*)(Args...) const>();
+    }
+
+    template <class R, class T, class... Args>
+    template <std::size_t ...Is>
+    r<mlab::bin_data> templated_command<R, T, Args...>::invoke_with_tuple(std::index_sequence<Is...>, deserialized_args_tuple_t<Args...> t) {
+        if (ka::target_method<R, T, Args...>::target == nullptr) {
+            return error::no_handler;
+        }
+        return serialize<R>(ka::target_method<R, T, Args...>::operator()(std::get<Is>(t)...));
+    }
+
+    template <class R, class T, class... Args>
+    templated_command<R, T, Args...>::templated_command(fn_ptr_t method, target_ptr_t ptr)
+        : command_base{signature_of(method)},
+          ka::target_method<R, T, Args...>{ptr, method}
+    {}
+
+    template <class R, class T, class... Args>
+    r<mlab::bin_data> templated_command<R, T, Args...>::command_response(mlab::bin_stream &s) {
+        if (auto args_tuple = deserialize<Args...>(s); args_tuple) {
+            return invoke_with_tuple(std::index_sequence_for<Args...>{}, std::move(*args_tuple));
+        } else {
+            return error::parsing_error;
+        }
+    }
+
+    template <class R, class T, class... Args>
+    r<std::string_view> bridge::register_command(R (T::*method)(Args...), std::string uuid, T *handler) {
+        return register_command(uuid.empty() ? signature_of(method) : std::move(uuid),
+                                std::make_unique<templated_command<R, T, Args...>>(method, handler));
+    }
+
+    template <class R, class T, class... Args>
+    r<std::string_view> bridge::register_command(R (T::*method)(Args...) const, std::string uuid, T const *handler) {
+        return register_command(uuid.empty() ? signature_of(method) : std::move(uuid),
+                                std::make_unique<templated_command<R, T, Args...>>(method, handler));
+    }
+
+    template <class R, class T, class... Args>
+    r<std::string_view> bridge::lookup_uuid(R (T::*method)(Args...)) const {
+        return lookup_uuid(signature_of(method));
+    }
+
+    template <class R, class T, class... Args>
+    r<std::string_view> bridge::lookup_uuid(R (T::*method)(Args...) const) const {
+        return lookup_uuid(signature_of(method));
+    }
+
+    template <class R, class T, class... Args>
+    r<> bridge::remote_supports(R (T::*method)(Args...) const, std::string_view uuid) const {
+        const auto sign = signature_of(method);
+        if (const auto r = remote_get_signature(uuid.empty() ? sign : std::string{uuid}); not r) {
+            return r.error();
+        } else if (sign != *r) {
+            return error::mismatching_signature;
+        }
+        return mlab::result_success;
+    }
+
+    template <class R, class T, class... Args>
+    r<> bridge::remote_supports(R (T::*method)(Args...), std::string_view uuid) const {
+        const auto sign = signature_of(method);
+        if (const auto r = remote_get_signature(uuid.empty() ? sign : std::string{uuid}); not r) {
+            return r.error();
+        } else if (sign != *r) {
+            return error::mismatching_signature;
+        }
+        return mlab::result_success;
+    }
+
+
+    template <class R, class T, class... Args>
+    r<R> bridge::remote_invoke_unique(R (T::*method)(Args...) const, Args... args) {
+        if (const auto r = lookup_uuid(method); not r) {
+            return r.error();
+        } else {
+            return remote_invoke(method, *r, std::forward<Args>(args)...);
+        }
+    }
+
+    template <class R, class T, class... Args>
+    r<R> bridge::remote_invoke_unique(R (T::*method)(Args...), Args... args) {
+        if (const auto r = lookup_uuid(method); not r) {
+            return r.error();
+        } else {
+            return remote_invoke(method, *r, std::forward<Args>(args)...);
+        }
+    }
+
+
+    template <class R, class T, class... Args>
+    r<R> bridge::remote_invoke(R (T::*method)(Args...) const, std::string_view uuid, Args... args) {
+        if (const auto r_invoke = remote_invoke(uuid, serialize<Args...>(std::forward<Args>(args)...)); r_invoke) {
+            mlab::bin_stream s{*r_invoke};
+            if (const auto r = deserialize<R>(s); r) {
+                return std::move(std::get<0>(*r));
+            } else {
+                return error::parsing_error;
+            }
+        } else {
+            return r_invoke.error();
+        }
+    }
+
+    template <class R, class T, class... Args>
+    r<R> bridge::remote_invoke(R (T::*method)(Args...), std::string_view uuid, Args... args) {
+        if (const auto r_invoke = remote_invoke(uuid, serialize<Args...>(std::forward<Args>(args)...)); r_invoke) {
+            mlab::bin_stream s{*r_invoke};
+            if (const auto r = deserialize<R>(s); r) {
+                return std::move(std::get<0>(*r));
+            } else {
+                return error::parsing_error;
+            }
+        } else {
+            return r_invoke.error();
+        }
+    }
+}
+
+#endif//KEYCARD_ACCESS_RPC_HPP
---
+++ keycard-access/lib/libka/src/ka/rpc.cpp
@@ -0,0 +1,172 @@
+//
+// Created by spak on 7/18/23.
+//
+
+#include <ka/rpc.hpp>
+
+namespace ka::rpc {
+
+    namespace {
+        enum struct command_type : std::uint8_t {
+            none = 0x00,
+            query_signature,
+            user_command = 0xff
+        };
+    }
+
+    command_base::command_base(std::string signature_) : signature{std::move(signature_)} {}
+
+    bridge::bridge(std::unique_ptr<bridge_remote_interface> remote_if, std::unique_ptr<bridge_local_interface> local_if)
+        : _remote_if{std::move(remote_if)}, _local_if{std::move(local_if)} {}
+
+    r<std::string_view> bridge::register_command(std::string uuid, std::unique_ptr<command_base> cmd) {
+        auto it = _cmds.lower_bound(uuid);
+        if (it != std::end(_cmds) and it->first == uuid) {
+            ESP_LOGE("RPC", "Duplicate command uuid %s", uuid.data());
+            return error::invalid_argument;
+        }
+        return std::string_view{_cmds.insert(it, std::make_pair(std::move(uuid), std::move(cmd)))->first};
+    }
+
+
+    r<mlab::bin_data> bridge::command_response(mlab::bin_data const &payload) const {
+        if (_remote_if == nullptr) {
+            return error::transport_error;
+        } else {
+            return _remote_if->command_response(payload);
+        }
+    }
+
+    r<> bridge::serve_loop() {
+        _serve_stop = false;
+        if (_local_if == nullptr) {
+            return error::transport_error;
+        }
+        while (not _serve_stop) {
+            error e{};
+            if (auto r_rcv = _local_if->receive(); r_rcv) {
+                if (auto r_rsp = local_invoke(*r_rcv); r_rsp) {
+                    if (auto r_txf = _local_if->send(*r_rsp); r_txf) {
+                        continue;
+                    } else {
+                        e = r_txf.error();
+                    }
+                } else {
+                    e = r_rsp.error();
+                }
+            } else {
+                e = r_rcv.error();
+            }
+            // Is the error recoverable?
+            if (e == error::channel_error or e == error::transport_error) {
+                return e;
+            }
+        }
+        return mlab::result_success;
+    }
+
+    void bridge::serve_stop() {
+        _serve_stop = true;
+    }
+
+    r<std::string> bridge::remote_get_signature(std::string_view uuid) const {
+        mlab::bin_data payload;
+        payload << mlab::prealloc(uuid.size() + 6)
+                << command_type::query_signature
+                << mlab::length_encoded << uuid;
+        if (const auto r_cmd = command_response(payload); r_cmd) {
+            mlab::bin_stream s{*r_cmd};
+            if (auto opt_sign = deserialize<std::string>(s); opt_sign) {
+                return std::move(std::get<0>(*opt_sign));
+            } else {
+                return error::parsing_error;
+            }
+        } else {
+            return r_cmd.error();
+        }
+    }
+
+    r<mlab::bin_data> bridge::remote_invoke(std::string_view uuid, mlab::bin_data const &body) {
+        if (auto it = _cmds.find(std::string{uuid}); it != std::end(_cmds)) {
+            mlab::bin_data payload;
+            payload << mlab::prealloc(uuid.size() + 5 + body.size())
+                    << command_type::user_command
+                    << mlab::length_encoded << uuid
+                    << body;
+            return command_response(payload);
+        } else {
+            return error::invalid_argument;
+        }
+    }
+
+    r<mlab::bin_data> bridge::local_invoke(mlab::bin_data const &packed_cmd) const {
+        mlab::bin_stream s{packed_cmd};
+        auto cmd_type = command_type::none;
+        std::string uuid{};
+        s >> cmd_type >> mlab::length_encoded >> uuid;
+        if (s.bad()) {
+            return error::transport_error;
+        }
+        switch (cmd_type) {
+            case command_type::none:
+                return error::transport_error;
+            case command_type::user_command:
+                return local_invoke(uuid, s);
+            case command_type::query_signature:
+                if (auto sign = get_signature(uuid); sign.empty()) {
+                    return error::unknown_command;
+                } else {
+                    return serialize<std::string_view>(sign);
+                }
+        }
+        return error::transport_error;
+    }
+
+    r<mlab::bin_data> bridge::local_invoke(std::string_view uuid, mlab::bin_stream &s) const {
+        if (auto it = _cmds.find(std::string{uuid}); it != std::end(_cmds)) {
+            return it->second->command_response(s);
+        } else {
+            return error::unknown_command;
+        }
+    }
+
+    bool bridge::contains(std::string_view uuid) const {
+        return _cmds.find(std::string{uuid}) != std::end(_cmds);
+    }
+
+    r<std::string_view> bridge::lookup_uuid(std::string_view signature) const {
+        auto retval_it = std::end(_cmds);
+        for (auto it = std::begin(_cmds); it != std::end(_cmds); ++it) {
+            if (it->second->signature == signature) {
+                if (retval_it == std::end(_cmds)) {
+                    retval_it = it;
+                } else {
+                    // Multiple signatures found
+                    return error::invalid_argument;
+                }
+            }
+        }
+        if (retval_it == std::end(_cmds)) {
+            return error::mismatching_signature;
+        }
+        return std::string_view{retval_it->first};
+    }
+
+    std::string_view bridge::get_signature(std::string_view uuid) const {
+        if (auto it = _cmds.find(std::string{uuid}); it != std::end(_cmds)) {
+            return it->second->signature;
+        }
+        return {};
+    }
+
+    struct bar {
+        int foo(std::string_view) { return 32; }
+    };
+
+    void baz() {
+        rpc::templated_command<int, bar, std::string_view> cmd{&bar::foo};
+
+        bridge b{};
+        b.register_command(&bar::foo);
+    }
+}// namespace ka::rpc
