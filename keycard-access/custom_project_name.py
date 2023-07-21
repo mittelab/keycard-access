e2352108d00e8827ae92780034cb9b3534842752
Pietro Saccardi <lizardm4@gmail.com>
2023-07-21 23:46:56 +0200

    Allow to not specify the project option

--- keycard-access/custom_project_name.py
+++ keycard-access/custom_project_name.py
@@ -11,7 +11,7 @@ Import('env')
 
 def main():
     project_dir = env['PROJECT_DIR']
-    project_name = env.GetProjectOption("custom_project_name")
+    project_name = env.GetProjectOption("custom_project_name", None)
     if not project_name:
         project_name = 'keycard-access'
     project_name_txt = os.path.join(project_dir, 'project_name.txt')
