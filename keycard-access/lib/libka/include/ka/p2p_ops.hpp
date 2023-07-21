c7eb99fdc159f2a67e0adb582ee795ffd5d0761a
Pietro Saccardi <lizardm4@gmail.com>
2023-07-21 15:41:37 +0200

    Implement full remote-local gate interface over RPC

--- keycard-access/lib/libka/include/ka/p2p_ops.hpp
+++ keycard-access/lib/libka/include/ka/p2p_ops.hpp
@@ -45,6 +45,100 @@ namespace ka::p2p {
         bool operational = false;
     };
 
+    namespace v2 {
+
+        enum struct error : std::uint8_t {
+            unauthorized = 0,
+            invalid_argument,
+            invalid_operation
+        };
+
+        [[nodiscard]] const char *to_string(error e);
+
+        template <class ...Args>
+        using r = mlab::result<error, Args...>;
+
+        class local_gate {
+            gate &_g;
+            std::shared_ptr<secure_initiator> _sec_layer;
+            rpc::bridge _b;
+
+            [[nodiscard]] r<> assert_peer_is_keymaker(bool allow_unconfigured = false) const;
+            [[nodiscard]] pub_key peer_pub_key() const;
+        public:
+            explicit local_gate(gate &g, std::shared_ptr<secure_initiator> initiator);
+
+            void serve_loop();
+
+            /**
+             * @addtogroup RemoteMethods
+             * @{
+             */
+
+            [[nodiscard]] fw_info get_fw_info() const;
+            [[nodiscard]] gate_update_config get_update_settings() const;
+            [[nodiscard]] gate_wifi_status get_wifi_status() const;
+            [[nodiscard]] update_status is_updating() const;
+            [[nodiscard]] gpio_responder_config get_gpio_config() const;
+            [[nodiscard]] std::string get_backend_url() const;
+            [[nodiscard]] gate_registration_info get_registration_info() const;
+
+            [[nodiscard]] r<release_info> check_for_updates();
+            [[nodiscard]] r<gate_base_key> register_gate(gate_id requested_id);
+
+            r<> set_update_settings(std::string_view update_channel, bool automatic_updates);
+            r<> update_manually(std::string_view fw_url);
+            r<> set_backend_url(std::string_view url, std::string_view api_key);
+            r<> set_gpio_config(gpio_responder_config cfg);
+            r<> reset_gate();
+            r<release_info> update_now();
+            r<bool> connect_wifi(std::string_view ssid, std::string_view password);
+
+            void disconnect();
+
+            /**
+             * @}
+             */
+        };
+
+        class remote_gate {
+            std::shared_ptr<secure_target> _sec_layer;
+            mutable rpc::bridge _b;
+
+        public:
+            explicit remote_gate(std::shared_ptr<secure_target> target);
+
+            /**
+             * @addtogroup RemoteMethods
+             * @{
+             */
+
+            [[nodiscard]] rpc::r<fw_info> get_fw_info() const;
+            [[nodiscard]] rpc::r<gate_update_config> get_update_settings() const;
+            [[nodiscard]] rpc::r<gate_wifi_status> get_wifi_status() const;
+            [[nodiscard]] rpc::r<update_status> is_updating() const;
+            [[nodiscard]] rpc::r<gpio_responder_config> get_gpio_config() const;
+            [[nodiscard]] rpc::r<std::string> get_backend_url() const;
+            [[nodiscard]] rpc::r<gate_registration_info> get_registration_info() const;
+
+            [[nodiscard]] rpc::r<r<release_info>> check_for_updates();
+            [[nodiscard]] rpc::r<r<gate_base_key>> register_gate(gate_id requested_id);
+
+            rpc::r<r<>> set_update_settings(std::string_view update_channel, bool automatic_updates);
+            rpc::r<r<>> update_manually(std::string_view fw_url);
+            rpc::r<r<>> set_backend_url(std::string_view url, std::string_view api_key);
+            rpc::r<r<>> set_gpio_config(gpio_responder_config cfg);
+            rpc::r<r<>> reset_gate();
+            rpc::r<r<release_info>> update_now();
+            rpc::r<r<bool>> connect_wifi(std::string_view ssid, std::string_view password);
+
+            rpc::r<> disconnect();
+            /**
+             * @}
+             */
+        };
+    }
+
     enum struct error : std::uint8_t {
         malformed,           ///< Command is malformed or unsupported
         unauthorized,        ///< This keymaker is not allowed to issue this command.
--- keycard-access/lib/libka/src/ka/p2p_ops.cpp
+++ keycard-access/lib/libka/src/ka/p2p_ops.cpp
@@ -18,6 +18,317 @@
 #undef MLAB_RESULT_LOG_PREFIX
 #define MLAB_RESULT_LOG_PREFIX TAG
 
+namespace ka::rpc {
+    template <> struct use_default_serialization<ka::pub_key> : std::true_type {};
+    template <> struct use_default_serialization<ka::gate_id> : std::true_type {};
+    template <> struct use_default_serialization<ka::fw_info> : std::true_type {};
+    template <> struct use_default_serialization<ka::p2p::gate_update_config> : std::true_type {};
+    template <> struct use_default_serialization<ka::p2p::gate_wifi_status> : std::true_type {};
+    template <> struct use_default_serialization<ka::p2p::gate_registration_info> : std::true_type {};
+    template <> struct use_default_serialization<ka::update_status> : std::true_type {};
+    template <> struct use_default_serialization<ka::gpio_responder_config> : std::true_type {};
+    template <> struct use_default_serialization<ka::release_info> : std::true_type {};
+    template <> struct use_default_serialization<ka::gate_base_key> : std::true_type {};
+
+    template <class T>
+    struct serializer<ka::p2p::v2::r<T>> {
+        static void serialize(mlab::bin_data &bd, ka::p2p::v2::r<T> const &r) {
+            bd << bool(r);
+            if (r) {
+                serializer<T>::serialize(bd, *r);
+            } else {
+                bd << r.error();
+            }
+        }
+
+        [[nodiscard]] static ka::p2p::v2::r<T> deserialize(mlab::bin_stream &s) {
+            bool is_ok = false;
+            s >> is_ok;
+            if (not s.bad()) {
+                if (is_ok) {
+                    return serializer<T>::deserialize(s);
+                } else {
+                    ka::p2p::v2::error e{};
+                    s >> e;
+                    return e;
+                }
+            }
+            return ka::p2p::v2::error{};
+        }
+    };
+
+    template <>
+    struct serializer<ka::p2p::v2::r<>> {
+        static void serialize(mlab::bin_data &bd, ka::p2p::v2::r<> const &r) {
+            bd << bool(r);
+            if (not r) {
+                bd << r.error();
+            }
+        }
+
+        [[nodiscard]] static ka::p2p::v2::r<> deserialize(mlab::bin_stream &s) {
+            bool is_ok = false;
+            s >> is_ok;
+            if (not s.bad()) {
+                if (is_ok) {
+                    return mlab::result_success;
+                } else {
+                    ka::p2p::v2::error e{};
+                    s >> e;
+                    return e;
+                }
+            }
+            return ka::p2p::v2::error{};
+        }
+    };
+}
+
+namespace ka::p2p::v2 {
+
+    const char *to_string(error e) {
+        switch (e) {
+            case error::unauthorized:
+                return "unauthorized";
+            case error::invalid_argument:
+                return "invalid argument";
+            case error::invalid_operation:
+                return "invalid operation";
+        }
+        return "UNKNOWN";
+    }
+
+    local_gate::local_gate(gate &g, std::shared_ptr<secure_initiator> initiator)
+        : _g{g},
+          _sec_layer{std::move(initiator)},
+          _b{std::make_unique<initiator_bridge_interface>(_sec_layer)}
+    {
+        _b.register_command(&local_gate::get_fw_info, *this);
+        _b.register_command(&local_gate::get_update_settings, *this);
+        _b.register_command(&local_gate::get_wifi_status, *this);
+        _b.register_command(&local_gate::is_updating, *this);
+        _b.register_command(&local_gate::get_gpio_config, *this);
+        _b.register_command(&local_gate::get_backend_url, *this);
+        _b.register_command(&local_gate::get_registration_info, *this);
+        _b.register_command(&local_gate::check_for_updates, *this);
+        _b.register_command(&local_gate::register_gate, *this);
+        _b.register_command(&local_gate::set_update_settings, *this);
+        _b.register_command(&local_gate::update_manually, *this);
+        _b.register_command(&local_gate::set_backend_url, *this);
+        _b.register_command(&local_gate::set_gpio_config, *this);
+        _b.register_command(&local_gate::reset_gate, *this);
+        _b.register_command(&local_gate::update_now, *this);
+        _b.register_command(&local_gate::connect_wifi, *this);
+    }
+
+    void local_gate::serve_loop() {
+        _b.serve_loop();
+    }
+
+    pub_key local_gate::peer_pub_key() const {
+        if (_sec_layer == nullptr) {
+            ESP_LOGE(TAG, "Not connected.");
+            return pub_key{};
+        }
+        return _sec_layer->peer_pub_key();
+    }
+
+    fw_info local_gate::get_fw_info() const {
+        return fw_info::get_running_fw();
+    }
+
+    gate_update_config local_gate::get_update_settings() const {
+        return gate_update_config{std::string{_g.update_channel()}, _g.updates_automatically()};
+    }
+
+    gate_wifi_status local_gate::get_wifi_status() const {
+        if (const auto ssid = _g.wifi_get_ssid(); ssid) {
+            return gate_wifi_status{*ssid, _g.wifi_test()};
+        }
+        return gate_wifi_status{"", false};
+    }
+
+    update_status local_gate::is_updating() const {
+        return _g.is_updating();
+    }
+
+    gpio_responder_config local_gate::get_gpio_config() const {
+        return gpio_responder_config::get_global_config();
+    }
+
+    std::string local_gate::get_backend_url() const {
+        ESP_LOGE(TAG, "get_backend_url not yet implemented");
+        return "";
+    }
+
+    gate_registration_info local_gate::get_registration_info() const {
+        return gate_registration_info{_g.public_info(), _g.keymaker_pk()};
+    }
+
+    r<release_info> local_gate::check_for_updates() {
+        TRY(assert_peer_is_keymaker(true));
+        if (auto ri = _g.check_for_updates(); ri) {
+            return std::move(*ri);
+        }
+        return release_info{};
+    }
+
+    r<gate_base_key> local_gate::register_gate(gate_id requested_id) {
+        if (_g.is_configured()) {
+            return error::invalid_operation;
+        }
+        if (const auto bk = _g.configure(requested_id, peer_pub_key()); not bk) {
+            return error::invalid_operation;
+        } else {
+            return *bk;
+        }
+    }
+
+    r<> local_gate::set_update_settings(std::string_view update_channel, bool automatic_updates) {
+        TRY(assert_peer_is_keymaker(true));
+        _g.set_update_automatically(automatic_updates);
+        if (not update_channel.empty() and not _g.set_update_channel(update_channel, true)) {
+            return error::invalid_argument;
+        }
+        return mlab::result_success;
+    }
+
+    r<> local_gate::update_manually(std::string_view fw_url) {
+        TRY(assert_peer_is_keymaker(true));
+        auto body = [from = std::string{fw_url}, &g = _g]() {
+            g.update_manually(from);
+        };
+        std::thread upd_th{body};
+        return mlab::result_success;
+    }
+
+    r<> local_gate::set_backend_url(std::string_view, std::string_view) {
+        TRY(assert_peer_is_keymaker());
+        ESP_LOGE(TAG, "set_backend_url not yet implemented");
+        return error::invalid_operation;
+    }
+
+    r<> local_gate::set_gpio_config(gpio_responder_config cfg) {
+        TRY(assert_peer_is_keymaker());
+        if (not gpio_responder_config::set_global_config(cfg)) {
+            return error::invalid_argument;
+        }
+        return mlab::result_success;
+    }
+
+    r<> local_gate::reset_gate() {
+        TRY(assert_peer_is_keymaker());
+        _g.reset();
+        return mlab::result_success;
+    }
+
+    r<release_info> local_gate::update_now() {
+        TRY(assert_peer_is_keymaker(true));
+        if (const auto ri = _g.check_for_updates(); ri) {
+            auto body = [from = ri->firmware_url, &g = _g]() {
+                g.update_manually(from);
+            };
+            std::thread upd_th{body};
+            return *ri;
+        } else {
+            return release_info{};
+        }
+    }
+
+    r<bool> local_gate::connect_wifi(std::string_view ssid, std::string_view password) {
+        TRY(assert_peer_is_keymaker(true));
+        return _g.wifi_connect(ssid, password);
+    }
+
+    r<> local_gate::assert_peer_is_keymaker(bool allow_unconfigured) const {
+        if (_g.is_configured()) {
+            if (peer_pub_key() != _g.keymaker_pk()) {
+                return error::unauthorized;
+            }
+        } else if (not allow_unconfigured) {
+            return error::invalid_operation;
+        }
+        return mlab::result_success;
+    }
+
+    void local_gate::disconnect() {
+        _b.serve_stop();
+    }
+
+
+    remote_gate::remote_gate(std::shared_ptr<secure_target> target)
+        : _sec_layer{std::move(target)},
+          _b{std::make_unique<target_bridge_interface>(_sec_layer)}
+    {}
+
+    rpc::r<fw_info> remote_gate::get_fw_info() const {
+        return _b.remote_invoke_unique(&local_gate::get_fw_info);
+    }
+
+    rpc::r<gate_update_config> remote_gate::get_update_settings() const {
+        return _b.remote_invoke_unique(&local_gate::get_update_settings);
+    }
+
+    rpc::r<gate_wifi_status> remote_gate::get_wifi_status() const {
+        return _b.remote_invoke_unique(&local_gate::get_wifi_status);
+    }
+
+    rpc::r<update_status> remote_gate::is_updating() const {
+        return _b.remote_invoke_unique(&local_gate::is_updating);
+    }
+
+    rpc::r<gpio_responder_config> remote_gate::get_gpio_config() const {
+        return _b.remote_invoke_unique(&local_gate::get_gpio_config);
+    }
+
+    rpc::r<std::string> remote_gate::get_backend_url() const {
+        return _b.remote_invoke_unique(&local_gate::get_backend_url);
+    }
+
+    rpc::r<gate_registration_info> remote_gate::get_registration_info() const {
+        return _b.remote_invoke_unique(&local_gate::get_registration_info);
+    }
+
+    rpc::r<r<release_info>> remote_gate::check_for_updates() {
+        return _b.remote_invoke_unique(&local_gate::check_for_updates);
+    }
+
+    rpc::r<r<gate_base_key>> remote_gate::register_gate(gate_id requested_id) {
+        return _b.remote_invoke_unique(&local_gate::register_gate, requested_id);
+    }
+
+    rpc::r<r<>> remote_gate::set_update_settings(std::string_view update_channel, bool automatic_updates) {
+        return _b.remote_invoke_unique(&local_gate::set_update_settings, update_channel, automatic_updates);
+    }
+
+    rpc::r<r<>> remote_gate::update_manually(std::string_view fw_url) {
+        return _b.remote_invoke_unique(&local_gate::update_manually, fw_url);
+    }
+
+    rpc::r<r<>> remote_gate::set_backend_url(std::string_view url, std::string_view api_key) {
+        return _b.remote_invoke_unique(&local_gate::set_backend_url, url, api_key);
+    }
+
+    rpc::r<r<>> remote_gate::set_gpio_config(gpio_responder_config cfg) {
+        return _b.remote_invoke_unique(&local_gate::set_gpio_config, cfg);
+    }
+
+    rpc::r<r<>> remote_gate::reset_gate() {
+        return _b.remote_invoke_unique(&local_gate::reset_gate);
+    }
+
+    rpc::r<r<release_info>> remote_gate::update_now() {
+        return _b.remote_invoke_unique(&local_gate::update_now);
+    }
+
+    rpc::r<r<bool>> remote_gate::connect_wifi(std::string_view ssid, std::string_view password) {
+        return _b.remote_invoke_unique(&local_gate::connect_wifi, ssid, password);
+    }
+
+    rpc::r<> remote_gate::disconnect() {
+        return _b.remote_invoke_unique(&local_gate::disconnect);
+    }
+}
+
 namespace ka::p2p {
     namespace bits {
         static constexpr std::uint8_t command_hello = 0x00;
